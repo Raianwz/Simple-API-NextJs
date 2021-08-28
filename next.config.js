caeb786e4081ac70a51b43d724fc2de1162497ac
Raianwz <raian03@live.com>
2021-08-27 22:49:24 -0300

    Flocoins

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
         { source: '/dolar/', destination: '/api/dolar/' },
         { source: '/smlurker/:user*/', destination: '/api/smlurker/:user*/' },
         { source: '/bonecoins/:bones*/', destination: '/api/bonecoins/:bones*/' },
+        { source: '/flocoins/:flocos*/', destination: '/api/flocoins/:flocos*/' },
         { source: '/lirocoins/:liro*/', destination: '/api/lirocoins/:liro*/' },
         { source: '/suncoins/:suns*/', destination: '/api/suncoins/:suns*/' },
       ],
