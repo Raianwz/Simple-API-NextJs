08f9cc73724925e5e80951f5166d172845129d67
Raianwz <raian03@live.com>
2021-08-27 22:02:56 -0300

    Rewrites e Suncoins

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
     trailingSlash: true,
+    rewrites: async () => [
+        { source: '/dolar/', destination: '/api/dolar/' },
+        { source: '/smlurker/:user*/', destination: '/api/smlurker/:user*/' },
+        { source: '/bonecoins/:bones*/', destination: '/api/bonecoins/:bones*/' },
+        { source: '/lirocoins/:liro*/', destination: '/api/lirocoins/:liro*/' },
+        { source: '/suncoins/:suns*/', destination: '/api/suncoins/:suns*/' },
+      ],
 }
