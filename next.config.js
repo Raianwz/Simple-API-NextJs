d32338d9a7e99ecb0b39bca053fbb1b70b849d59
Raianwz <raian03@live.com>
2021-08-30 13:59:08 -0300

    JoséCoins✅

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-    trailingSlash: true,
-    rewrites: async () => [
-        { source: '/dolar/', destination: '/api/dolar/' },
-        { source: '/smlurker/:user*/', destination: '/api/smlurker/:user*/' },
-        { source: '/bonecoins/:bones*/', destination: '/api/bonecoins/:bones*/' },
-        { source: '/flocoins/:flocos*/', destination: '/api/flocoins/:flocos*/' },
-        { source: '/lirocoins/:liro*/', destination: '/api/lirocoins/:liro*/' },
-        { source: '/suncoins/:suns*/', destination: '/api/suncoins/:suns*/' },
-      ],
+  trailingSlash: true,
+  rewrites: async () => [
+    { source: '/dolar/', destination: '/api/dolar/' },
+    { source: '/smlurker/:user*/', destination: '/api/smlurker/:user*/' },
+    { source: '/bonecoins/:bones*/', destination: '/api/bonecoins/:bones*/' },
+    { source: '/flocoins/:flocos*/', destination: '/api/flocoins/:flocos*/' },
+    { source: '/josecoins/:zes*/', destination: '/api/josecoins/:zes*/' },
+    { source: '/lirocoins/:liro*/', destination: '/api/lirocoins/:liro*/' },
+    { source: '/suncoins/:suns*/', destination: '/api/suncoins/:suns*/' },
+  ],
 }
