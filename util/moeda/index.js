060abbe3b9486f722cad20172aa2fd2573fde44f
Raianwz <raian03@live.com>
2021-09-25 23:11:38 -0300

    Fetch Catch error add

--- util/moeda/index.js
+++ util/moeda/index.js
@@ -4,8 +4,8 @@ const msg = (texto) => JSON.stringify(texto).replace(/[\\"]/g, '')
 export default async function coins(TOKEN, user, valor, res) {
     let resp; TOKEN = TOKEN ?? '';
     if (TOKEN.lenght !== 0 && isNaN(user) && !isNaN(valor)) {
-        const Jebaited = await fetch(`https://api.jebaited.net/addPoints/${TOKEN}/${user}/${valor}`)
-        if (Jebaited && Jebaited.status == 200) {
+        const Jebaited = await fetch(`https://api.jebaited.net/addPoints/${TOKEN}/${user}/${valor}`).catch((err)=>{ return resp = res.end(msg(`:/ Desculpe @${user} ocorreu um erro e Você NÃO ganhou seus ${valor}`))})
+        if (Jebaited) {
             res.status('200')
             resp = res.end(msg(`@${user} ganhou ${valor}`))
         }
