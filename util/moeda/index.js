5ec3ca1d795a3bb1a822fa59a540b0098bd32e9d
Raianwz <raian03@live.com>
2021-09-25 22:53:25 -0300

    Try daily via Nightbot✅

--- util/moeda/index.js
+++ util/moeda/index.js
@@ -4,17 +4,17 @@ const msg = (texto) => JSON.stringify(texto).replace(/[\\"]/g, '')
 export default async function coins(TOKEN, user, valor, res) {
     let resp; TOKEN = TOKEN ?? '';
     if (TOKEN.lenght !== 0 && isNaN(user) && !isNaN(valor)) {
-        try {
-            const Jebaited = await fetch(`https://api.jebaited.net/addPoints/${TOKEN}/${user}/${valor}`)
-            Jebaited.status === 200 ? res.status('200') : res.status('400')
-            Jebaited.status === 200
-                ? resp = res.end(msg(`@${user} ganhou ${valor}`))
-                : resp = res.end(msg(`FeelsDankMan @${user} ocorreu um erro, Alô @RaianWz.`))
-        } catch (err) {
-            resp = res.end(msg(`FeelsDankMan @${user} ocorreu um erro, chamen o @Raianwz.`))
+        const Jebaited = await fetch(`https://api.jebaited.net/addPoints/${TOKEN}/${user}/${valor}`)
+        if (Jebaited && Jebaited.status == 200) {
+            res.status('200')
+            resp = res.end(msg(`@${user} ganhou ${valor}`))
+        }
+        else{
+            res.status('400')
+            resp = res.end(msg(`!ndl @${user} ${valor} Tentando pelo @Nightbot — Você ganhou ${valor}`))
         }
         return resp
     }
-    res.status('404')
-    return resp = res.end(msg(`FeelsDankMan @${user} algo deu MUITO errado mesmo, chamen @Raianwz.`))
+    resp = res.end(msg(`:/ Desculpe @${user} ocorreu um erro e Você NÃO ganhou seus ${valor}`))
+    return resp
 }
