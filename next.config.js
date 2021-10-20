e49eb2aa1ca344f22f1758b3fe7c29293350a2fb
Raianwz <raian03@live.com>
2021-10-19 21:36:25 -0300

    Update Fix Random Chatters

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   trailingSlash: true,
   rewrites: async () => [
     { source: '/dolar/', destination: '/api/dolar/' },
-    { source: '/rd/chatters/:canal*/', destination: '/api/randomchatter/:canal*/' },
+    { source: '/rd/chatters/:canal*/', destination: '/api/random/chatters/:canal*/' },
     { source: '/smlurker/:user*/', destination: '/api/smlurker/:user*/' },
     { source: '/bonecoins/:bones*/', destination: '/api/bonecoins/:bones*/' },
     { source: '/flocoins/:flocos*/', destination: '/api/flocoins/:flocos*/' },
