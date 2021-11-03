5abe3be5018643b08ac6328e1a2375143452d987
Raianwz <raian03@live.com>
2021-11-03 16:57:20 -0300

    Update SMLurker getUser

--- pages/api/smlurker/[...user].js
+++ pages/api/smlurker/[...user].js
@@ -5,7 +5,7 @@ export default async function users(req, res) {
     const { user } = req.query
     const TCLIENTID = process.env.TCLIENTID
     const THELIX = process.env.THELIX
-    const username = user[0]
+    const username = user[0];
     if (username != null || username != "") {
         const ivr = await fetch(`https://api.ivr.fi/twitch/resolve/${username}`)
         const Helix = await fetch(`https://api.twitch.tv/helix/users?login=${username}`, {
@@ -15,26 +15,25 @@ export default async function users(req, res) {
                 'Client-ID': TCLIENTID,
             }
         });
-
-        if (Helix.status === 200) {
-            const twitch = await Helix.json(), UserData = await ivr.json()
-            const resp = {
-                "id": twitch.data[0].id,
-                "login": twitch.data[0].login,
-                "display_name": twitch.data[0].display_name,
-                "chatColor": UserData.status == 200 ? UserData.chatColor : "#9148FF",
-                "profile_image_url": twitch.data[0].profile_image_url,
-                "offline_image_url": twitch.data[0].offline_image_url,
+        if (Helix.status === 200 || ivr.status == 200) {
+            if (Helix.status === 200) {
+                const twitch = await Helix.json(), UserData = await ivr.json()
+                var resp = {};
+                resp = {"id": twitch.data[0].id,"login": twitch.data[0].login,
+                    "display_name": twitch.data[0].display_name,
+                    "chatColor": UserData.status == 200 ? UserData.chatColor : "#9148FF",
+                    "profile_image_url": twitch.data[0].profile_image_url,
+                    "offline_image_url": twitch.data[0].offline_image_url,}
+            }else if(ivr.status === 200){
+                const UserData = await ivr.json()
+                resp = {"id": UserData.id, "login": UserData.login,"display_name": UserData.displayName,
+                "chatColor": UserData.chatColor,"profile_image_url": UserData.logo}
             }
             res.status('200')
             res.end(JSON.stringify(resp))
-        } else {
-            res.status('401')
-            res.end(JSON.stringify('Algo deu errado =/'))
+            return
         }
-        return
+        res.status('404')
+        res.end(JSON.stringify('Algo deu errado =/'))
     }
-    res.status('404')
-    res.end(JSON.stringify('Algo deu errado =/'))
-}
-
+}   
