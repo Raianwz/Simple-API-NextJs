d7138cd8d0108406b7fe553ce365c0446e7dd877
Raianwz <Raian03@live.com>
2022-02-22 10:45:39 -0300

    Update index.js

--- util/moeda/index.js
+++ util/moeda/index.js
@@ -7,10 +7,10 @@ export default async function coins(TOKEN, user, valor, res) {
 
     if (TOKEN.lenght !== 0 && isNaN(user) && Number(valor)) {
         valor >= 200 && valor <= 800 ? valor *= 2 : false
-        const Jebaited = fetch(`https://api.jebaited.net/addPoints/${TOKEN}/${user}/${valor}`)
+        /* const Jebaited = fetch(`https://api.jebaited.net/addPoints/${TOKEN}/${user}/${valor}`)
             .then(rsp => { return rsp })
-            .catch((err) => { return resp = res.end(msg(`:/ Desculpe @${user} ocorreu um erro e Você NÃO ganhou seus ${valor}`)) })
-        // const Jebaited = await fetch(`https://api.jebaited.net/addPoints/${TOKEN}/${user}/${valor}`).catch((err)=>{ return resp = res.end(msg(`:/ Desculpe @${user} ocorreu um erro e Você NÃO ganhou seus ${valor}`))})
+            .catch((err) => { return resp = res.end(msg(`:/ Desculpe @${user} ocorreu um erro e Você NÃO ganhou seus ${valor}`)) }) */
+        const Jebaited = await fetch(`https://api.jebaited.net/addPoints/${TOKEN}/${user}/${valor}`).catch((err)=>{ return resp = res.end(msg(`:/ Desculpe @${user} ocorreu um erro e Você NÃO ganhou seus ${valor}`))})
         if (Jebaited) {
             res.status('200')
             resp = res.end(msg(`@${user} ganhou ${valor}`))
