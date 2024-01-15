f2f34274e753eb92eb3e157bdd5a0c13160a81d5
raianwz <raian03@live.com>
2024-01-14 22:10:18 -0300

    Updated Coin Dinamic

--- pages/api/moeda/[...coin].js
+++ pages/api/moeda/[...coin].js
@@ -1,25 +1,32 @@
 import matchList from "@/pages/api/moeda/match.json";
-import helpList from "@/pages/api/moeda/help.json"
+import path from 'path';
+import { promises as fs } from 'fs';
+const jsonDirectory = path.join(process.cwd(), 'pages/api/moeda/');
+
+
 
 export default async function Moedas(req, res) {
     const coin = req.query.coin[0]
     const emote = req.query.coin[1] ?? 'elementsOkay'
     const isJson = checkJson(req.query.coin[2])
     const combina = String(coin).toLocaleUpperCase()
+    const localMatchList = await fs.readFile(jsonDirectory + '/match.json', 'utf8');
     const msg = (txt) => isJson == false ? txt.replace(/[\\"]/g, '') : txt
-
     res.setHeader('Content-Type', 'application/json')
-
-    if(isNaN(combina)){
-        if(combina.toLocaleLowerCase() == 'all'){
-            res.end(msg(JSON.stringify(matchList)))
+    
+    
+    if (isNaN(combina)) {
+        if (combina.toLocaleLowerCase() == 'all') {
+            res.end(localMatchList)
         }
-        if(matchList[combina]){
+        if (matchList[combina] || localMatchList[combina]) {
             let moeda = await getCurrency(combina, emote)
+            console.log(moeda)
             res.end(msg(JSON.stringify(moeda)))
         }
-        if(combina.toLocaleLowerCase() == 'help'){
-            res.end(JSON.stringify(helpList))
+        if (combina.toLocaleLowerCase() == 'help') {
+            const localHelpList = await fs.readFile(jsonDirectory + '/help.json', 'utf8');
+            res.end(localHelpList)
         }
     }
     res.status(400).json({
@@ -30,30 +37,32 @@ export default async function Moedas(req, res) {
     })
 }
 
-async function getCurrency(comb, emote){
+async function getCurrency(comb, emote) {
     const getCoin = await fetch(`https://economia.awesomeapi.com.br/all/${comb}`)
-    const respCoin =  await getCoin.json()
-    const localCoin = respCoin[String(comb).slice(0,3)]
+    const respCoin = await getCoin.json()
+    const localCoin = respCoin[String(comb).slice(0, 3)]
     const rp = (valor) => valor.replace('.', ',')
     let saida = "", simb = "R$"
     localCoin.codein !== "BRL" ? simb = localCoin.codein : false;
+    console.log(comb)
+    console.log(saida)
 
     let dia, mes, ano, vData = localCoin.create_date, nData;
-        dia = vData.slice(8,10)
-        mes = vData.slice(5,7)
-        ano = vData.slice(0,4)
-        nData = `${dia}/${mes}/${ano} ${vData.slice(11,19)}`
+    dia = vData.slice(8, 10)
+    mes = vData.slice(5, 7)
+    ano = vData.slice(0, 4)
+    nData = `${dia}/${mes}/${ano} ${vData.slice(11, 19)}`
 
-    return saida = `${String(localCoin.name).replace('/', ' em ')} está valendo ${simb} ${rp(Number(localCoin.bid).toFixed(2))}💵 (${rp(localCoin.bid)}) ➖ Max. ${simb} ${rp(localCoin.high)} ⬆ ➖ Min. ${simb} ${rp(localCoin.low)} ⬇ ➖ ${String(emote)} Dados de ${nData}`  
+    return saida = `${String(localCoin.name).replace('/', ' em ')} está valendo ${simb} ${rp(Number(localCoin.bid).toFixed(2))}💵 (${rp(localCoin.bid)}) ➖ Max. ${simb} ${rp(localCoin.high)} ⬆ ➖ Min. ${simb} ${rp(localCoin.low)} ⬇ ➖ ${String(emote)} Dados de ${nData}`
 }
 
 
-function checkJson(text){
+function checkJson(text) {
     let localText = String(text).toLocaleLowerCase()
 
-    if(localText === "true"){
+    if (localText === "true") {
         return true
-    }   
+    }
 
     return false
 }
