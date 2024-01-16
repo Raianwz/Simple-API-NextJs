0a4b67086364a5437bb5fe800de0a5606c6fe0dc
raianwz <raian03@live.com>
2024-01-16 09:14:24 -0300

    Updated Coin and check API

--- pages/api/moeda/[...coin].js
+++ pages/api/moeda/[...coin].js
@@ -1,8 +1,7 @@
-const JsonMoedas = await fetch('https://json-wz.vercel.app/moedas')
-let matchList = await JsonMoedas.json(); matchList = matchList[0];
+
+
 
 export default async function Moedas(req, res) {
-    
     const coin = req.query.coin[0]
     const emote = req.query.coin[1] ?? 'elementsOkay'
     const isJson = checkJson(req.query.coin[2])
@@ -12,16 +11,28 @@ export default async function Moedas(req, res) {
     res.setHeader('Content-Type', 'application/json')
 
     if (isNaN(combina)) {
-        if (matchList[combina]) {
-            let moeda = await getCurrency(combina, emote)
-            res.status(200).json(msg(JSON.stringify(moeda)))
+        const JsonMoedas = await fetch('https://json-wz.vercel.app/moedas')
+        if (JsonMoedas.status === 200) {
+            let matchList = await JsonMoedas.json();
+            matchList = matchList[0];
+            if (matchList[combina]) {
+                let moeda = await getCurrency(combina, emote)
+                res.status(200).json(msg(JSON.stringify(moeda)))
+            }
+        }else{
+            res.status(404).json({
+                message: "Algo deu errado 🫤",
+                status: 404,
+                error: "Not Found"
+            })
         }
     }
-    res.status(400).json({
+    res.status(404).json({
         message: "Verifique as combinações possiveis em /docs/moeda",
         error: "Not Found",
         status: 404,
     })
+    res.end()
 }
 
 async function getCurrency(comb, emote) {
